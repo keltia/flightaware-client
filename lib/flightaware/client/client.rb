75cee93aa0fc297855a156706a2e71d8a1f11c23
Ollivier Robert <roberto@keltia.net>
2015-02-16 14:46:18 +0100

    Add counters.

--- lib/flightaware/client/client.rb
+++ lib/flightaware/client/client.rb
@@ -11,7 +11,12 @@ module FlightAware
   class Client
     include Celluloid::IO
 
+    attr_reader :bytes
+    attr_reader :pkts
+
     def initialize(config)
+      @bytes = 0
+      @pkts = 0
       puts("Connecting to #{config.site}:#{config.port} using TLS.")
       raw_socket = TCPSocket.new(config.site, config.port)
       puts("  Initiating TLS negociation")
@@ -28,6 +33,8 @@ module FlightAware
     def run
       buf = @ssl.read
       puts(buf)
+      @bytes += buf.size
+      @pkts += 1
     end
 
     def wait
